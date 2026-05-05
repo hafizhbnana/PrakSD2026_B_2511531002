5df5dcf3536abedd475333dd27267fd1c8da289c
hafizhbnana <hafizhhabibullahbeniska@gmail.com>
2026-05-05 09:24:18 +0700

    Update IterasiQueue_2511531002.java

--- pekan4_2511531002/IterasiQueue_2511531002.java
+++ pekan4_2511531002/IterasiQueue_2511531002.java
@@ -1,5 +1,21 @@
 package pekan4_2511531002;
 
-public class IterasiQueue_2511531002 {
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Iterator;
 
+public class IterasiQueue_2511531002 {
+	public static void main(String[] args) {
+		Queue<String> q_1002 = new LinkedList<>();
+		
+		q_1002.add("Praktikum");
+		q_1002.add("Struktur");
+		q_1002.add("Data");
+		q_1002.add("Dan");
+		q_1002.add("Algoritma");
+		Iterator<String> iterator = q_1002.iterator();
+		while (iterator.hasNext()) {
+			System.out.print(iterator.next() + " ");
+		}
+	}
 }
